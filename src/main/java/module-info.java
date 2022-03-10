4f289769b0b82f1db3e758f0c7138f29997b6f60
Roman Shchekotov <rom.shchekotov@gmail.com>
2022-03-10 22:34:43 +0100

    feat: deprecated swing ui, switch to jfx ✨

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+open module org.ryan.dictionary {
+  requires javafx.base;
+  requires javafx.graphics;
+  requires javafx.controls;
+
+  requires java.logging;
+
+  requires com.google.gson;
+  requires lombok;
+}
